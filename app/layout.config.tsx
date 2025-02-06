45b46d9446cb11af7dfe4a979527f9ce7a79b704
GonzaDDV <gonzalodiazdevivar@gmail.com>
2025-02-06 01:15:02 -0300

    initial docs

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
  * Shared layout configurations
@@ -7,16 +7,42 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
+
+export const logo = (
+  <>
+    <img
+      className="w-16 md:w-20 lg:w-24 dark:hidden"
+      src={"/images/stream-light.svg"}
+      alt="Stream Finance logo"
+    />
+
+    <img
+      className="w-16 md:w-20 lg:w-24 hidden dark:block"
+      src={"/images/stream-dark.svg"}
+      alt="Stream Finance logo"
+    />
+  </>
+);
+
 export const baseOptions: BaseLayoutProps = {
   nav: {
     // can be JSX too!
-    title: 'My App',
+    title: <>{logo}</>,
   },
   links: [
     {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
+      text: "Documentation",
+      url: "/",
+      active: "nested-url",
+    },
+    {
+      text: "Web App",
+      url: "https://v2.streamprotocol.money",
+    },
+    {
+      text: "Dashboard",
+      url: "https://dashboard-v2.streamprotocol.money",
     },
   ],
+  githubUrl: "https://github.com/StreamDefi/contracts",
 };
